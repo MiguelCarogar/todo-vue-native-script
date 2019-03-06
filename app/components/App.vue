9c0c3365c34548c1e1ab2a1ba7a5ee3fd2bd38e7
Miguel Angel Caro Garcia <miguel@factorial.io>
2019-03-06 11:12:27 +0100

    Adding loader

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,20 +1,28 @@
 <template>
   <Page class="page">
-    <ActionBar title="My Tasks" class="action-bar" />
+    <ActionBar title="My Tasks" class="action-bar">
+      <ActionItem @tap="onTapShare"
+        ios.systemIcon="9" ios.position="left"
+        android.systemIcon="ic_menu_share" android.position="actionBar" 
+      />   
+    </ActionBar>
 
     <TabView height="100%" androidTabsPosition="bottom" selectedTabTextColor="#53ba82" tabTextFontSize="15" >
+         
       <TabViewItem title="To Do" textTransform="uppercase" >
         <!-- Positions an input field, a button, and the list of tasks in a vertical stack. -->
         <StackLayout orientation="vertical" width="100%" height="100%">
-
+          <StackLayout>
+            <ActivityIndicator horizontalAlignment="center" top="50" width="10" height="50" :busy="isBusy" />
+          </StackLayout>
           <GridLayout columns="2*,*" rows="*" width="100%" height="25%">
             <!-- Configures the text field and ensures that pressing Return on the keyboard produces the same result as tapping the button. -->
             <TextField col="0" row="0" v-model="textFieldValue" hint="Type new task..." editable="true" @returnPress="onButtonTap" /> 
 
-            <Button col="1" row="0" text="Add task" @tap="onButtonTap" />
+            <Button col="1" row="0" text="Add task" @tap="onAddTask" />
           </GridLayout>
 
-          <ListView class="list-group" for="todo in todos" @itemTap="onItemTap" style="height:75%" separatorColor="transparent" >
+          <ListView class="list-group" for="todo in todos" @itemTap="onItemTap" style="height:75%" separatorColor="transparent" > 
             <v-template>
               <Label id="active-task" :text="todo.name" class="list-group-item-heading" textWrap="true" />
             </v-template>
@@ -22,11 +30,16 @@
         </StackLayout>
       </TabViewItem>
       <TabViewItem title="Completed" textTransform="uppercase">
-        <ListView class="list-group" for="done in dones" @itemTap="onDoneTap" style="height:75%" separatorColor="transparent">
-          <v-template>
-            <Label id="completed-task" :text="done.name" class="list-group-item-heading" textWrap="true" />
-          </v-template>
-        </ListView>
+        <StackLayout orientation="vertical" width="100%" height="100%">
+          <StackLayout>
+            <ActivityIndicator horizontalAlignment="center" top="50" width="10" height="50" :busy="isBusy" />
+          </StackLayout>
+          <ListView class="list-group" for="done in dones" @itemTap="onDoneTap" style="height:75%" separatorColor="transparent">
+            <v-template>
+              <Label id="completed-task" :text="done.name" class="list-group-item-heading" textWrap="true" />
+            </v-template>
+          </ListView>
+        </StackLayout>
       </TabViewItem>
     </TabView>
   </Page>
@@ -36,6 +49,7 @@
 export default {
   methods: {
     onItemTap: function(args) {
+      this.isBusy = true;
       action('What do you want to do with this task?', 'Cancel', ['Mark completed', 'Delete forever']) 
         .then(result => { 
           console.log(result); // Logs the selected option for debugging.
@@ -43,38 +57,50 @@ export default {
             case 'Mark completed': 
               this.dones.unshift(args.item); // Places the tapped active task at the top of the completed tasks.
               this.todos.splice(args.index, 1); // Removes the tapped active  task.
+              this.isBusy = false;
               break;
             case 'Delete forever':
               this.todos.splice(args.index, 1); // Removes the tapped active task.
+              this.isBusy = false;
               break; 
             case 'Cancel' || undefined: // Dismisses the dialog
+              this.isBusy = false;
               break; 
           }
         })
     },
 
-    onButtonTap() {
+    onTapShare() {
+      console.log('Back');
+    },
+
+    onAddTask() {
       if (this.textFieldValue === "") return; // Prevents users from entering an empty string.
       console.log("New task added: " + this.textFieldValue + "."); // Logs the newly added task in the console for debugging.
       this.todos.unshift({ name: this.textFieldValue }); // Adds tasks in the ToDo array. Newly added tasks are immediately shown on the screen.
       this.textFieldValue = ""; // Clears the text field so that users can start adding new tasks immediately.
     },
 
-    onDoneTap: function(args) { 
+    onDoneTap: function(args) {
+      this.isBusy = true;
       action('What do you want to do with this task?', 'Cancel', ['Mark to do', 'Delete forever'])
         .then(result => { 
           console.log(result); // Logs the selected option for debugging. 
           switch (result) { 
             case 'Mark to do':
               this.todos.unshift(args.item); // Places the tapped completed task at the top of the to do tasks. 
-              this.dones.splice(args.index,1); // Removes the tapped completed task. 
+              this.dones.splice(args.index,1); // Removes the tapped completed task.
+              this.isBusy = false;
               break; 
             case 'Delete forever': 
-              this.dones.splice(args.index, 1); // Removes the tapped completed task. 
+              this.dones.splice(args.index, 1); // Removes the tapped completed task.
+              this.isBusy = false;
               break; 
             case 'Cancel' || undefined: // Dismisses the dialog 
+              this.isBusy = false;
               break; 
-          } 
+          }
+          this.isBusy= false;
         }) 
     },
   },
@@ -85,6 +111,8 @@ export default {
       todos: [],
 
       textFieldValue: "",
+
+      isBusy: false,
 
     };
   },
